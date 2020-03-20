1a62b2a9e33bfdf832d1eab8b9577b3ee41a8900
AdarshFez <as30kuma@siena.edu>
2020-03-19 21:14:07 -0400

    Home Page
    started adding Home Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
+import {NativeScriptHttpClientModule} from "nativescript-angular/http-client";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { ItemsComponent } from "./item/items.component";
 import { ItemDetailComponent } from "./item/item-detail.component";
+import { HomeComponent } from "./home/home.compont";
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
 // import { NativeScriptFormsModule } from "nativescript-angular/forms";
@@ -18,12 +20,14 @@ import { ItemDetailComponent } from "./item/item-detail.component";
     ],
     imports: [
         NativeScriptModule,
-        AppRoutingModule
+        AppRoutingModule,
+        NativeScriptHttpClientModule
     ],
     declarations: [
         AppComponent,
         ItemsComponent,
-        ItemDetailComponent
+        ItemDetailComponent,
+        HomeComponent
     ],
     providers: [],
     schemas: [
--- src/app/item/item.service.ts
+++ src/app/item/item.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 import { Item } from "./item";
+
 
 @Injectable({
     providedIn: "root"
@@ -9,19 +11,24 @@ import { Item } from "./item";
 
 export class ItemService {
 
-    private count = 1;
+    private count = 0;
+    // { id: 1, name: "Sam Philip", supplies: "paste",addres: "4 clifton park rd, Albany, 12211",time: 4, phone: 8458484848 },
+    constructor (private http: HttpClient)
     private items = new Array<Item>(
-        { id: 1, name: "Sam Philip", supplies: "paste",addres: "4 clifton park rd, Albany, 12211",time: 4, phone: 8458484848 },
-
     );
+    addItems ( Name: string, Material: string, address: string, time: string, phone: number ): void {
+        const item = {Name, Material, address, time, phone};
+        this.http.put('https://goldenboys-e429e.firebaseio.com/items.json', item).subscribe();
+            // {Name, Material, address, time, phone});
 
+    }
 
     getItems(): Array<Item> {
         return this.items;
     }
 
-    getItem(id: number): Item {
-        return this.items.filter((item) => item.id === id)[0];
+    getItem(Material: string): Item {
+        return this.items.filter((item) => item.supplies === Material)[0];
     }
 
     getItemCount(): number {
