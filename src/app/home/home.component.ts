9982160a3a294c50a0d3ff57042b4447b341fe53
AdarshFez <as30kuma@siena.edu>
2020-03-24 21:16:53 -0400

    functionality on action and points
    added scoll and started changing the dataService

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 
-import { DataService } from "../shared/dataS.service";
+import { DataService } from "../shared/data.service";
 import { DataItem } from "../shared/data.supply";
 
 @Component({
--- src/app/shared/data.service.ts
+++ src/app/shared/data.service.ts
@@ -1,14 +1,7 @@
 import { Injectable } from "@angular/core";
 
-export interface DataItem {
-    id: number;
-    name: string;
-    description: string;
-    address: string;
-    time: number;
-    phone: number;
-
-}
+import { HttpClient } from "@angular/common/http";
+import { DataItem } from "./data.supply";
 
 @Injectable({
     providedIn: "root"
@@ -26,14 +19,24 @@ export class DataService {
         },
         {
             id: 2,
-            name: "corn John",
+            name: "cornnn John",
             description: "Paper",
             address: "32 sddf",
             time: 8,
             phone: 4728955
+        },
+        {
+            id: 3,
+            name: "ham Jon",
+            description: "tooth bursh",
+            address: "48 main",
+            time: 2,
+            phone: 234345
         }
 
     );
+
+    constructor(private http: HttpClient) {}
 
     getItems(): Array<DataItem> {
         return this.items;
@@ -41,5 +44,22 @@ export class DataService {
 
     getItem(id: number): DataItem {
         return this.items.filter((item) => item.id === id)[0];
+    }
+
+    makeItem(nam: string, des: string, add: string, tim: number, phone: number): void {
+        // tslint:disable-next-line: no-object-literal-type-assertion
+        const temp: any = <DataItem>{};
+        temp.name = nam;
+        temp.description = des;
+        temp.address = add;
+        temp.time = tim;
+        temp.phone = phone;
+        // const temp =  (1, nam, des, add, tim, phone );
+        // <DataItem> = (1 , nam , des ; add ; tim ; phone);
+        this.http.put("https://goldenboys-be087.firebaseio.com/supplies.json", temp).subscribe();
+    }
+
+    serverGet(): void {
+        const temp = 1 + 1;
     }
 }
